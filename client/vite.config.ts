6a5ed504b452c0ab7b773a8257a63bb9e02f1078
Cheever Esler <ames.esler@gmail.com>
2023-02-21 15:43:12 -0400

    add testing library, further configure vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {},
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
 });
